9a92bcaa0fb6b84c49df7eb5baa52377944a5057
Jérémy MARC <jeremy.marc@circulae.com>
2026-03-28 23:57:19 +0100

    Fixing manifest icons

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -28,10 +28,17 @@ export const metadata: Metadata = {
     icons: {
         icon: [
             { url: "/favicon.ico" },
-            { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-            { url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" }
-        ],
-        apple: [{ url: "/icons/apple-touch-icon.png", sizes: "180x180" }]
+            {
+                url: "/icons/favicon-192.png",
+                sizes: "192x192",
+                type: "image/png"
+            },
+            {
+                url: "/icons/favicon-512.png",
+                sizes: "512x512",
+                type: "image/png"
+            }
+        ]
     }
 };
 
